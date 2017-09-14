8d1025d3e56be8f515984f40a8a171ab5a7ed34a
cnpoe <cnpoek@gmail.com>
2017-09-13 23:44:30 -0500

    función para comenzar el juego

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 var tileImages = [];
+var startButton = document.getElementById('start');
+
+startButton.addEventListener('click', startGame);
+
 for (var x = 0; x < 7; x++) {
 	tileImages.push( x+'.jpg');
 }
-console.log(tileImages);
+
+
+function startGame(){
+	startButton.style.display = 'none';
+	console.log('started');
+}
