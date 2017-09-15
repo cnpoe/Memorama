ad73b4cf3b0180558c8c721b5079fe74aa9028f6
cnpoe <cnpoek@gmail.com>
2017-09-14 23:59:29 -0500

    avance

--- script.js
+++ script.js
@@ -3,12 +3,32 @@ var startButton = document.getElementById('start');
 
 startButton.addEventListener('click', startGame);
 
-for (var x = 0; x < 7; x++) {
-	tileImages.push( x+'.jpg');
-}
-
 
 function startGame(){
 	startButton.style.display = 'none';
+	if (!gamePlay) {
+		gamePlay = true;
+		buildArray();
+		titleArray = titleImages.concat(titleImages);
+		shuffleArray(titleArray);
+		console.log(titleArray);
+	}
 	console.log('started');
+}
+
+function buildArray() {
+	for (var x = 0; x < 7; x++) {
+		tileImages.push( x+'.jpg');
+	}
+}
+
+function shuffleArray(array){
+	for (var x = array.length -1; x > 0; x--) {
+		var holder = Math.floor(Math.random() * (x+1));
+		var itemValue = array[x];
+		array[x] = array[holder];
+		array[holder] = itemValue;
+		console.log(holder);
+	}
+	return array;
 }
